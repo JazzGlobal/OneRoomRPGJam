d34fef515e1792a2e2cd0a6dc3dc0a74d93445ca
JazzGlobal <waymer147@gmail.com>
2018-02-18 23:50:03 -0500

    Added PlayerWalkingState.cs

---
+++ OneRoomRPGJam/Entity/PlayerStates/PlayerWalkingState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OneRoomRPGJam
+{
+	public class PlayerWalkingState : State
+	{
+		public PlayerWalkingState()
+		{
+		}
+
+		public override void Init()
+		{
+		}
+
+		public override void OnEnter()
+		{
+		}
+
+		public override void OnExit()
+		{
+		}
+
+		public override void Render(SpriteBatch spriteBatch)
+		{
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+		}
+	}
+}
